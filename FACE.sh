dea400b8d3d75c7f404433451d91654d81fc2063
Benay Tomas <115767219+benaytms@users.noreply.github.com>
2024-10-07 14:32:51 -0300

    face shell file update

--- FACE.sh
+++ FACE.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 SOURCE_FOLDER="./files"
-# this is where the images you want to identify faces go
+# THIS IS WHERE YOU SAVE THE IMAGES YOU WANT TO TEST
 
 # you'll need to save them with the following standard: image_{number}
 # for instance if you want to use 3 images you'll save them as such:
 # image_1.jpg, image_2.jpg, image_3.jpg 
 # (as far as i know the extension can be any valid image format, jpg, png, webp, etc)
 
+SAVE_FOLDER="./recognized_files"
+# THIS IS WHERE THE FILES WILL BE SAVED AFTER THE ALGORITHM IDENTIFY THE FACES
+# i suggest you leave it as it is and if you want you can copy from there
+
 PYTHON_SCRIPT="./scripts/FACE2.py"
 # this is the python script with the face recognition algorithm
-
 # you don't need to change this (unless you want to use the previous version, FACE1.py, which i don't recommend)
 
-SAVE_FOLDER="./recognized_files"
-# this is where the files will be saved after you use the script
-
-# i suggest you leave it as it is and if you want you can copy from there
+mkdir -p "$SOURCE_FOLDER"
+mkdir -p "$SAVE_FOLDER"
 
 for img in $(ls "$SOURCE_FOLDER"/image_* | sort -V); do
     # this will go through the source_folder, using the pattern image_*
